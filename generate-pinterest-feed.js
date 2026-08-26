4a3c57c6610b375564e5b967dd6ba777ebb8d53b
Ronak Amin <anokhiada.sona@gmail.com>
2026-08-26 13:46:35 -0400

    Add files via upload

--- generate-pinterest-feed.js
+++ generate-pinterest-feed.js
@@ -143,6 +143,7 @@ const COLOR_WORDS = [
 // Up to three colours, separated by "/", is what Google accepts for a
 // multi-coloured piece.
 const COLOR_OVERRIDES = {
+  'red-bandhani-top-and-white-straight-pants': 'Red/White',
   'rani-pink-short-kurta-and-wide-leg-pants': 'Pink',
   'color-blocked-silk-anarkali-set': 'Purple/Red/Orange',
   'vintage-hand-embroidered-chaniya-choli': 'Mustard/Red',
